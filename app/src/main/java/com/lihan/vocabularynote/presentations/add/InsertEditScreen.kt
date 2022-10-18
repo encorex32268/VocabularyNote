e0dfe806fbaf3247bd7ca61b531ab0a97d995a24
LiHan <encorex32268@gmail.com>
2022-10-18 16:13:53 +0900

    Home Screen Item Ok .

---
+++ app/src/main/java/com/lihan/vocabularynote/presentations/add/InsertEditScreen.kt
@@ -0,0 +1,14 @@
+package com.lihan.vocabularynote.presentations.add
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun InsertEditScreen(
+    modifier: Modifier = Modifier,
+    noteId : Int ,
+) {
+
+
+}
--- app/src/main/java/com/lihan/vocabularynote/presentations/home/components/VocabularyNoteItem.kt
+++ app/src/main/java/com/lihan/vocabularynote/presentations/home/components/VocabularyNoteItem.kt
@@ -1,5 +1,7 @@
 package com.lihan.vocabularynote.presentations.home.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,30 +10,48 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lihan.vocabularynote.domain.model.VocabularyNote
+import org.w3c.dom.Text
 
 @Composable
 fun VocabularyNoteItem(
     modifier: Modifier = Modifier,
     vocabularyNote: VocabularyNote,
-    onItemClick : (VocabularyNote) -> Unit = {}
+    onItemClick : (VocabularyNote) -> Unit = {},
+    typeCircleSize : Dp = 40.dp,
+    noteCardHeight : Dp = 150.dp,
+    hiraganaFontSize : TextUnit = 12.sp,
+    wordFontSize : TextUnit = 20.sp
 ) {
+    val rotated by remember {
+        mutableStateOf(false)
+    }
+    val rotation by animateFloatAsState(
+        targetValue = if (rotated) 180f else 0f,
+        animationSpec = tween(500)
+    )
+
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(150.dp)
             .padding(16.dp)
-            .background(
-                color = Color.LightGray,
-                shape = RoundedCornerShape(10.dp)
-            )
+            .graphicsLayer {
+                rotationY = rotation
+                cameraDistance = 8 * density
+            }
             .clickable {
                 onItemClick(vocabularyNote)
             }
@@ -40,17 +60,22 @@ fun VocabularyNoteItem(
     ) {
         Box(
             modifier = Modifier
-                .size(40.dp)
+                .padding(8.dp)
+                .size(typeCircleSize)
                 .clip(CircleShape)
                 .background(Color.Green)
-                .padding(8.dp)
                 .align(Alignment.TopStart)
         )
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(60.dp)
-                .padding(8.dp),
+                .padding(8.dp)
+                .height(noteCardHeight)
+                .background(
+                    Color.White,
+                    RoundedCornerShape(20.dp)
+                )
+            ,
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -60,19 +85,19 @@ fun VocabularyNoteItem(
             if (isShowSmallHiraganaText){
                 Text(
                     text = vocabularyNote.hiraganaOrKatakana,
-                    fontSize = 12.sp,
+                    fontSize = hiraganaFontSize,
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier.height(2.dp))
                 Text(
                     text = vocabularyNote.word,
-                    fontSize = 20.sp,
+                    fontSize = wordFontSize,
                     fontWeight = FontWeight.Bold
                 )
             }else{
                 Text(
                     text = vocabularyNote.hiraganaOrKatakana,
-                    fontSize = 20.sp,
+                    fontSize = hiraganaFontSize,
                     fontWeight = FontWeight.Bold
                 )
             }
